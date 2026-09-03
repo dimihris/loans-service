7e2bb53d91617bc0a37a48cb7bf0e67292060fbe
dimihris <hristo_dimitrov@flutterint.com>
2026-09-03 13:25:32 +0300

    feat: add global exception handler, and validation for LoanDTO and LoanController

--- src/main/java/com/dimihris/loansservice/controller/LoanController.java
+++ src/main/java/com/dimihris/loansservice/controller/LoanController.java
@@ -4,12 +4,15 @@ import com.dimihris.loansservice.constant.LoanConstants;
 import com.dimihris.loansservice.dto.LoanDto;
 import com.dimihris.loansservice.dto.response.ResponseDto;
 import com.dimihris.loansservice.service.LoanService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,13 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("api/v1/loans")
+@RequestMapping(value = "api/v1/loans", produces = MediaType.APPLICATION_JSON_VALUE)
+@Validated
 public class LoanController {
 
     private LoanService loanService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createLoan(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> createLoan(
+            @RequestParam
+            @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+            String mobileNumber) {
 
         loanService.createLoan(mobileNumber);
 
@@ -35,7 +42,10 @@ public class LoanController {
     }
 
     @GetMapping("/find")
-    public ResponseEntity<LoanDto> getLoanDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<LoanDto> getLoanDetails(
+            @RequestParam
+            @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+            String mobileNumber) {
 
         LoanDto loanDto = loanService.findLoanDetails(mobileNumber);
 
@@ -45,7 +55,7 @@ public class LoanController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateLoanDetails(@RequestBody LoanDto loanDto) {
+    public ResponseEntity<ResponseDto> updateLoanDetails(@RequestBody @Valid LoanDto loanDto) {
 
         boolean isUpdated = loanService.updateLoan(loanDto);
 
@@ -61,7 +71,10 @@ public class LoanController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteLoan(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteLoan(
+            @RequestParam
+            @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+            String mobileNumber) {
 
         boolean isDeleted = loanService.deleteLoan(mobileNumber);
 
--- src/main/java/com/dimihris/loansservice/dto/LoanDto.java
+++ src/main/java/com/dimihris/loansservice/dto/LoanDto.java
@@ -1,20 +1,31 @@
 package com.dimihris.loansservice.dto;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class LoanDto {
 
+    @NotEmpty(message = "Mobile Number can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile Number must be 10 digits")
     private String mobileNumber;
 
+    @NotEmpty(message = "Loan Number can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{12})",message = "LoanNumber must be 12 digits")
     private String loanNumber;
 
+    @NotEmpty(message = "LoanType can not be a null or empty")
     private String loanType;
 
+    @Positive(message = "Total loan amount should be greater than zero")
     private Integer totalLoan;
 
+    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than zero")
     private Integer paidAmount;
 
+    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than zero")
     private Integer outstandingAmount;
 }
