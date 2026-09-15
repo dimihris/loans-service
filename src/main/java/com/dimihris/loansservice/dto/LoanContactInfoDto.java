0935f2028b984e1e0db4dae70746435a06d25097
dimihris <hristo_dimitrov@flutterint.com>
2026-09-15 14:37:39 +0300

    refactor: LoanContactInfoDto from record to class

--- src/main/java/com/dimihris/loansservice/dto/LoanContactInfoDto.java
+++ src/main/java/com/dimihris/loansservice/dto/LoanContactInfoDto.java
@@ -1,14 +1,17 @@
 package com.dimihris.loansservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter @Setter
 @ConfigurationProperties(prefix = "loans")
-public record LoanContactInfoDto(
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport
-) {
+public class LoanContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
