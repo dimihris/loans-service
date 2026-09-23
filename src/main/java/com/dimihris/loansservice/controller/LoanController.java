f31418b4cdbc62a44c87f2ddbeb30e73afddcd2e
dimihris <hristo_dimitrov@flutterint.com>
2026-09-23 18:12:42 +0300

    implement cross cutting concerns, tracing and logging

--- src/main/java/com/dimihris/loansservice/controller/LoanController.java
+++ src/main/java/com/dimihris/loansservice/controller/LoanController.java
@@ -8,6 +8,8 @@ import com.dimihris.loansservice.service.LoanService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +37,8 @@ public class LoanController {
     private final Environment environment;
 
     private final LoanContactInfoDto loanContactIndoDto;
+
+    private static final Logger logger = LoggerFactory.getLogger(LoanController.class);
 
     @Value("${build.version}")
     private String buildVersion;
@@ -53,10 +58,10 @@ public class LoanController {
 
     @GetMapping("/find")
     public ResponseEntity<LoanDto> getLoanDetails(
-            @RequestParam
-            @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
-            String mobileNumber) {
+            @RequestHeader("eazybank-correlation-id") String correlationId,
+            @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") String mobileNumber) {
 
+        logger.debug("banking-app-corelation-id found: {}", correlationId);
         LoanDto loanDto = loanService.findLoanDetails(mobileNumber);
 
         return ResponseEntity
